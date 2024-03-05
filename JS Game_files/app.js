0a8f3dffee5a846dcf07f938583ea31eaf0f2e9b
nszandrew <viajantestemporais@gmail.com>
2024-03-05 18:47:08 -0300

    ate o 100

--- JS Game_files/app.js
+++ JS Game_files/app.js
@@ -1,11 +1,11 @@
-let numeroSecreto = parseInt(Math.random() * 11)
+let numeroSecreto = parseInt(Math.random() * 100)
 let tentativas = 1
 let chute
 
 alert('Boas vindas ao jogo do número secreto')
 
  while (chute != numeroSecreto) {
-     let chute = prompt('Escolha um número entre 1 e 10')
+     let chute = prompt('Escolha um número entre 1 e 100')
      if (chute == numeroSecreto) {
          break
      } else {
